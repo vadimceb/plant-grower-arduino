fd0d471c1920ad563dc8f6e894f6c049d63cb8fc
vadim.cebotari <vadim.cebotari@spindox.it>
2021-05-06 20:58:21 +0200

    Comment out wifi module

--- main/conf.h
+++ main/conf.h
@@ -2,7 +2,8 @@ char WIFI_SSID[] = "";
 char WIFI_PASSWORD[] = "";
 char SERVER_TO_SEND_DATA[] = "";
 
-int SPRAY_INTERVAL = 60000;
-int READ_DATA_TIME = 10000;
+int SPRAY_INTERVAL = 20000;
+int READ_DATA_TIME = 5000;
 
-int RELAY_PIN = 8;
+int RELAY_PIN_1 = 8;
+int RELAY_PIN_2 = 2;
--- main/main.ino
+++ main/main.ino
@@ -15,13 +15,13 @@ DFRobot_SHT20 sht20;
 
 // WIFI
 // Emulate Serial1 on pins 6/7 if not present
-#ifndef HAVE_HWSERIAL1
-#include "SoftwareSerial.h"
-SoftwareSerial Serial1(6, 7); // RX, TX
-#endif
+// #ifndef HAVE_HWSERIAL1
+// #include "SoftwareSerial.h"
+// SoftwareSerial Serial1(6, 7); // RX, TX
+// #endif
 
-int status = WL_IDLE_STATUS; // the Wifi radio's status
-WiFiEspClient client;        // Initialize the Ethernet client object
+// int status = WL_IDLE_STATUS; // the Wifi radio's status
+// WiFiEspClient client;        // Initialize the Ethernet client object
 
 void setup()
 {
@@ -29,8 +29,8 @@ void setup()
     Serial.println();
 
     // Setup timers for spraying and reading data
-    sprayTimer.set(SPRAY_INTERVAL);
     readTimer.set(READ_DATA_TIME);
+    sprayTimer.set(SPRAY_INTERVAL);
 
     // Setup connectors for data sensors
     /** Hardware Connections:
@@ -44,44 +44,48 @@ void setup()
     sht20.checkSHT20(); // Check SHT20 Sensor
 
     // Setup relays
-    pinMode(RELAY_PIN, OUTPUT);
+    pinMode(RELAY_PIN_1, OUTPUT);
+    pinMode(RELAY_PIN_2, OUTPUT);
+
+    // setupWifi();
 }
 
-void setupWifi()
-{
-    // initialize ESP module
-    WiFi.init(&Serial1);
+// void setupWifi()
+// {
+//     Serial1.begin(9600);
+//     // initialize ESP module
+//     WiFi.init(&Serial1);
 
-    // check for the presence of the shield
-    if (WiFi.status() == WL_NO_SHIELD)
-    {
-        Serial.println("WiFi shield not present");
-        // don't continue
-        while (true)
-        {
-        }
-    }
+//     // check for the presence of the shield
+//     if (WiFi.status() == WL_NO_SHIELD)
+//     {
+//         Serial.println("WiFi shield not present");
+//         // don't continue
+//         while (true)
+//         {
+//         }
+//     }
 
-    // attempt to connect to WiFi network
-    while (status != WL_CONNECTED)
-    {
-        Serial.print("Attempting to connect to WPA SSID: ");
-        Serial.println(WIFI_SSID);
-        // Connect to WPA/WPA2 network
-        status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    }
+//     // attempt to connect to WiFi network
+//     while (status != WL_CONNECTED)
+//     {
+//         Serial.print("Attempting to connect to WPA SSID: ");
+//         Serial.println(WIFI_SSID);
+//         // Connect to WPA/WPA2 network
+//         status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+//     }
 
-    Serial.println("You're connected to the network");
-    printWifiData();
-}
+//     Serial.println("You're connected to the network");
+//     printWifiData();
+// }
 
-void printWifiData()
-{
-    // print your WiFi shield's IP address
-    IPAddress ip = WiFi.localIP();
-    Serial.print("IP Address: ");
-    Serial.println(ip);
-}
+// void printWifiData()
+// {
+//     // print your WiFi shield's IP address
+//     IPAddress ip = WiFi.localIP();
+//     Serial.print("IP Address: ");
+//     Serial.println(ip);
+// }
 
 void loop()
 {
@@ -92,6 +96,7 @@ void loop()
 
     if (readTimer.repeat())
     {
+        Serial.println(">> readTimer");
         readSensorsData();
     }
 
@@ -103,9 +108,8 @@ void loop()
 
 void spray()
 {
-    //TODO: to be implemented..
-    // 1. Activate relays
-    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(RELAY_PIN_1, HIGH);
+    digitalWrite(RELAY_PIN_2, HIGH);
     relayTimer.set(500);
     relayTimer.start();
 }
@@ -114,15 +118,16 @@ void stopSpray()
 {
     relayTimer.stop();
     relayTimer.reset();
-    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(RELAY_PIN_1, LOW);
+    digitalWrite(RELAY_PIN_2, LOW);
 }
 
 void readSensorsData()
 {
     float temp = readTemperature();
     float humd = readHumidity();
-
-    sendData(temp, humd);
+    Serial.println("data|t"+String(temp)+";h"+String(humd)+";");
+    // sendData(0, humd);
 }
 
 float readTemperature()
@@ -137,18 +142,18 @@ float readHumidity()
     return humd;
 }
 
-bool sendData(float temp, float humd)
-{
-    int currTime = millis();
+// bool sendData(float temp, float humd)
+// {
+//     int currTime = millis();
 
-    //TODO: to be implemented
-    if (client.connect(SERVER_TO_SEND_DATA, 80))
-    {
-        Serial.println("Connected to server");
-        // Make a HTTP request
-        client.println("GET /asciilogo.txt HTTP/1.1");
-        client.println("Host: arduino.cc");
-        client.println("Connection: close");
-        client.println();
-    }
-}
+//     //TODO: to be implemented
+//     if (client.connect(SERVER_TO_SEND_DATA, 80))
+//     {
+//         Serial.println("Connected to server");
+//         // Make a HTTP request
+//         client.println("GET /asciilogo.txt HTTP/1.1");
+//         client.println("Host: arduino.cc");
+//         client.println("Connection: close");
+//         client.println();
+//     }
+// }
